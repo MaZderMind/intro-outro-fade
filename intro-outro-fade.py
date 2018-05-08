fe09fbff16d693e1d5a4593ddf3d89f57e66c7ab
MaZderMind <git@mazdermind.de>
2018-05-08 21:31:58 +0200

    args parsing

--- intro-outro-fade.py
+++ intro-outro-fade.py
@@ -11,10 +11,21 @@ GObject.threads_init()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--intro", help="intro to prepend")
+
+parser.add_argument("--intro-fade-time", help="duration in seconds for which intro and content should overlap", default=0.5)
+parser.add_argument("--intro-audio-earlier", help="duration in seconds by which the audio overlap between intro and content should be earlier then the video-overlap", default=0.25)
+
 parser.add_argument("--content", help="content to wrap", required=True)
-parser.add_argument("--outro", help="intro to append")
-parser.parse_args()
+
+parser.add_argument("--outro-fade-time", help="duration in seconds for which outro and content should overlap", default=0.5)
+parser.add_argument("--outro-audio-later", help="duration in seconds by which the audio overlap between outro and content should be later then the video-overlap", default=0.25)
+
+parser.add_argument("--outro", help="outro to append")
+
+args = parser.parse_args()
+
+print(args)
 
 
-mainloop = GObject.MainLoop()
-mainloop.run()
+#mainloop = GObject.MainLoop()
+#mainloop.run()
